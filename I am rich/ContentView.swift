40b4303e05d5d2b6c8c0ccf4c5c351593a7608fd
Peiyun <b612starnight@gmail.com>
2023-05-11 22:42:05 +0800

    Initial Commit

---
+++ I am rich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I am rich
+//
+//  Created by Peiyun on 2023/5/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
